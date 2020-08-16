a47933157eae6266974bee6e24fd0b76cd10dac6
Ravi Jiyani <jiyaniravi@gmail.com>
2020-08-16 21:20:13 +0530

    Test case should pass because test active profile and SgtPeppers profile is set to "dev"

--- src/test/java/com/af/soundsystem/CDPlayerTest.java
+++ src/test/java/com/af/soundsystem/CDPlayerTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("dev")
+@ActiveProfiles("prod")
 @ContextConfiguration(classes=CDPlayerConfig.class)
 public class CDPlayerTest {
 
