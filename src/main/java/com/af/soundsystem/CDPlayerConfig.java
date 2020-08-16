2e8d80263b85ee2b31115abc7e0d0c5c185fe9b8
Ravi Jiyani <jiyaniravi@gmail.com>
2020-08-16 21:14:45 +0530

    Test case should pass if environment has magic property

--- src/main/java/com/af/soundsystem/CDPlayerConfig.java
+++ src/main/java/com/af/soundsystem/CDPlayerConfig.java
@@ -2,8 +2,10 @@ package com.af.soundsystem;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:settings.properties")
 @ComponentScan
 public class CDPlayerConfig {
 
