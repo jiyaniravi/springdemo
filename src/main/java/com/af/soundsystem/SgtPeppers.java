704e37acf3aa0dda1ad5c9ad6449dea8158c197c
Ravi Jiyani <jiyaniravi@gmail.com>
2020-08-16 21:09:53 +0530

    Test should fail because SgtPappers bean should get created based on magic property's existence

--- src/main/java/com/af/soundsystem/SgtPeppers.java
+++ src/main/java/com/af/soundsystem/SgtPeppers.java
@@ -1,10 +1,12 @@
 package com.af.soundsystem;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("SgtPeppers")
+@Conditional(MagicExistsCondition.class)
 public class SgtPeppers implements CompactDisc{
 
     private String title = "Sgt. Peppers lonely Hearts club hand";
